0905a7e0573f75138115921fc04060b8620ef4bd
Robert Allred <rallred84@gmail.com>
2023-03-07 13:47:19 -0600

    Added function to add producers to right column

--- main.js
+++ main.js
@@ -31,17 +31,28 @@ const coffeeProducers = [
 
 //event listener for coffee button
 const coffeeImage = document.getElementById('coffee-pic');
+
 coffeeImage.addEventListener('click', () => {
   coffeeCount++;
-  let coffeeCountStatment = document.getElementById('coffee-count');
-  coffeeCountStatment.textContent = `Coffee: ${coffeeCount}`;
+  scoreboard.textContent = `Coffee: ${coffeeCount}`;
 });
 
-//adding a producer buy card
-for (i in coffeeProducers) {
-  const producerShop = document.getElementById('producer-shop');
-  let producer = document.createElement('div');
-  producer.innerHTML = `
+//event listener for scoreboard
+const scoreboard = document.querySelector('#coffee-count');
+
+//Not sure if this is the best way, but DOMSubtreeModified 'fires when the structure of the DOM tree changes'...so when the count changes on the scoreboard, this listener will fire (also worked with DOMNodeInserted and DOMNodeRemoved)
+scoreboard.addEventListener('DOMSubtreeModified', addProducer);
+
+//We want to make a function to make producer appear in producer list when the coffee score gets to half the producer's cost
+
+let activeProducers = [];
+
+function addProducer() {
+  for (i in coffeeProducers) {
+    const producerShop = document.getElementById('producer-shop');
+    if (coffeeCount >= coffeeProducers[i].cost / 2) {
+      let producer = document.createElement('div');
+      producer.innerHTML = `
 <div class="producer">
 <div class="producer-name">
   <span>${coffeeProducers[i].name}</span>
@@ -54,5 +65,28 @@ for (i in coffeeProducers) {
 </div>
 </div>
 `;
-  producerShop.appendChild(producer);
+      // Only want to append the producer's buy card (producer) if the producer does not already exist in the active producer list (activeProducers)
+      //if no active producers, add it
+      if (activeProducers.length === 0) {
+        producerShop.appendChild(producer);
+        activeProducers.push(coffeeProducers[i].name);
+      } else {
+        //If there are active producers, check to see if the current one matches any of them. Assumes it does not exist unless otherwise proven to. If it already exists, code will not run, if not code WILL run
+
+        //['Basic Coffee Roast Machine', 'French Press Machine']
+        let alreadyExists = false;
+        for (producerName of activeProducers) {
+          if (producerName === coffeeProducers[i].name) {
+            alreadyExists = true;
+          }
+        }
+        if (alreadyExists === false) {
+          producerShop.appendChild(producer);
+          activeProducers.push(coffeeProducers[i].name);
+        }
+      }
+    }
+  }
 }
+
+function appendProducer() {}
