e798ea645c599bb998a91059d96379f77352185c
Robert Allred <rallred84@gmail.com>
2023-03-06 23:49:43 -0600

    Initial commit-- basic html and css complete, plus adding producer card through js

---
+++ main.js
@@ -0,0 +1,57 @@
+//global variables
+let coffeeCount = 0;
+let coffeeRate = 0;
+
+const coffeeProducers = [
+  {
+    id: 0,
+    name: 'Basic Coffee Roast Machine',
+    quantity: 0,
+    rate: 1,
+    cost: 10,
+    isVisible: false,
+  },
+  {
+    id: 1,
+    name: 'French Press Machine',
+    quantity: 12,
+    rate: 2,
+    cost: 50,
+    isVisible: false,
+  },
+  {
+    id: 2,
+    name: 'Mr. Coffee Machine',
+    quantity: 4,
+    rate: 3,
+    cost: 100,
+    isVisible: false,
+  },
+];
+
+//event listener for coffee button
+const coffeeImage = document.getElementById('coffee-pic');
+coffeeImage.addEventListener('click', () => {
+  coffeeCount++;
+  let coffeeCountStatment = document.getElementById('coffee-count');
+  coffeeCountStatment.textContent = `Coffee: ${coffeeCount}`;
+});
+
+//adding a producer buy card
+let i = 0;
+const producerShop = document.getElementById('producer-shop');
+let producer = document.createElement('div');
+producer.innerHTML = `
+<div class="producer">
+<div class="producer-name">
+  <span>${coffeeProducers[i].name}</span>
+  <button>Buy</button>
+</div>
+<div class="producer-info">
+  <span>Quantity: ${coffeeProducers[i].quantity}</span>
+  <span>Coffee/Second: ${coffeeProducers[i].rate}</span>
+  <span>Cost: ${coffeeProducers[i].cost} coffee</span>
+</div>
+</div>
+`;
+producerShop.appendChild(producer);
